e5f5e970b85d3a2d88990b262ba66337ff6d7fbe
HusseinMayla <hssnmn1232game@gmail.com>
2026-05-19 23:52:21 +0300

    changed readme

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from app.extensions import db
+from app.auth.forms import LoginForm
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@auth_bp.route('/login')
+@auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return "Login Page"
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('main.index'))
+    return render_template('auth/login.html', title='Sign In', form=form)
 
 @auth_bp.route('/register')
 def register():
